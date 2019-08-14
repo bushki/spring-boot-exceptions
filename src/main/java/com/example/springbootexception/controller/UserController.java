a57d2d744b3457625ad2f772e3dbc6d6a7d7c2c1
Luis <luis.p.melgar@disney.com>
2019-08-14 16:13:32 -0700

    using ExceptionHandler annotation added page specific to math errors

--- src/main/java/com/example/springbootexception/controller/UserController.java
+++ src/main/java/com/example/springbootexception/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.springbootexception.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -10,4 +12,14 @@ public class UserController {
         int num = 10 / 0;
         return "add";
     }
+
+    @ExceptionHandler(value={java.lang.ArithmeticException.class})
+    public ModelAndView handlerArithmeticException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("mathError");
+        return modelAndView;
+    }
 }
+
+
