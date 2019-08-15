14e6c0249797d59c5755323a8355aaacab671734
Luis <luis.p.melgar@disney.com>
2019-08-14 21:57:24 -0700

    added single handler using SimpleMappingExceptionResolver

--- src/main/java/com/example/springbootexception/exceptions/GolbalExceptionHandler.java
+++ src/main/java/com/example/springbootexception/exceptions/GolbalExceptionHandler.java
@@ -6,19 +6,19 @@ import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GolbalExceptionHandler {
-    @ExceptionHandler(value={java.lang.ArithmeticException.class})
-    public ModelAndView handlerArithmeticException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("mathError");
-        return modelAndView;
-    }
-
-    @ExceptionHandler(value={java.lang.NullPointerException.class})
-    public ModelAndView handlerNullPointerException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("nullPointerError");
-        return modelAndView;
-    }
+//    @ExceptionHandler(value={java.lang.ArithmeticException.class})
+//    public ModelAndView handlerArithmeticException(Exception e) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject("exception", e.toString());
+//        modelAndView.setViewName("mathError");
+//        return modelAndView;
+//    }
+//
+//    @ExceptionHandler(value={java.lang.NullPointerException.class})
+//    public ModelAndView handlerNullPointerException(Exception e) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject("exception", e.toString());
+//        modelAndView.setViewName("nullPointerError");
+//        return modelAndView;
+//    }
 }
